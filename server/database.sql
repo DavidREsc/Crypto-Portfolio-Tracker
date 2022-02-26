acd730042ad36b11606566a844e4b09613a43bd7
David Rapala-Escorcia <david.rapalae@gmail.com>
2022-02-26 13:44:56 -0500

    Added form to browse assets with search filtering

--- server/database.sql
+++ server/database.sql
@@ -1,11 +1,38 @@
 CREATE DATABASE crypto_tracker;
 
 CREATE TABLE users(
-    user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
+    user_id UUID DEFAULT uuid_generate_v4(),
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE portfolios(
+    portfolio_id UUID DEFAULT uuid_generate_v4(),
+    user_id UUID NOT NULL,
+    portfolio_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (portfolio_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE assets(
+    asset_id SERIAL,
+    portfolio_id UUID NOT NULL,
+    asset_coin_id VARCHAR(255) NOT NULL,
+    asset_amount NUMERIC NOT NULL,
+    initial_price NUMERIC NOT NULL,
+    PRIMARY KEY (asset_id),
+    FOREIGN KEY (portfolio_id) REFERENCES portfolios(portfolio_id)
 );
 
 --insert users
 INSERT INTO users (user_email, user_password)
 VALUES ('david.rapala@gmail.com', 'gfg7fgdg32');
+
+--insert portfolios
+INSERT INTO portfolios (user_id, portfolio_name)
+VALUES ('82168349-0e4f-424e-87e3-a163e78deb81', 'Portfolio 1');
+
+--insert assets
+INSERT INTO assets (portfolio_id, asset_name, asset_amount)
+VALUES (3, 'Bitcoin', 9000);
--- server/routes/portfolio.js
+++ server/routes/portfolio.js
@@ -1,8 +1,52 @@
 const router = require('express').Router();
 const authorize = require('../middleware/authorize');
+const db = require('../db');
 
-router.get('/', (req, res) => {
-    res.status(200).send("ok");
-})
+router.get('/', authorize, async (req, res) => {
+    try {
+        const portfolios = await db.query(
+            'SELECT portfolios.portfolio_id, portfolios.portfolio_name FROM users ' + 
+            'LEFT JOIN portfolios ON users.user_id = portfolios.user_id ' +
+            'WHERE users.user_id = $1', [req.user]
+        );
+        const assets = await db.query(
+            'SELECT portfolios.portfolio_name, assets.asset_coin_id, assets.asset_amount, assets.initial_price FROM users ' +
+            'LEFT JOIN portfolios ON users.user_id = portfolios.user_id ' +
+            'INNER JOIN assets ON portfolios.portfolio_id = assets.portfolio_id ' +
+            'WHERE users.user_id = $1', [req.user]
+        );
+
+        res.status(200).json({"portfolios": portfolios.rows, "assets": assets.rows});
+
+    } catch (error) {
+        console.log("error")      
+    }
+});
+
+router.post('/create-portfolio', authorize, async (req, res) => {
+    const {name} = req.body;
+    try {
+        const portfolio = await db.query(
+            'INSERT INTO portfolios (user_id, portfolio_name) ' +
+            'VALUES ($1, $2) RETURNING *', [req.user, name]
+        );
+        res.status(200).json(portfolio);
+    } catch (error) {
+        console.log("error");
+    }
+});
+
+router.post('/add-asset', authorize, async (req, res) => {
+    const {coin_id, amount, portfolio_id, initial_price} = req.body;
+    try {
+        const asset = await db.query(
+            'INSERT INTO assets (portfolio_id, asset_coin_id, asset_amount, initial_price) ' +
+            'VALUES ($1, $2, $3, $4) RETURNING *', [portfolio_id, coin_id, amount, initial_price]
+        );
+        res.status(200).json(asset);
+    } catch (error) {
+        console.log(error);      
+    }
+});
 
 module.exports = router;
